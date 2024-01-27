a8c7ea928339b1e91a1600338da4c15cb6d56932
Brian Williams <152632135+bkwilliamsmerncode@users.noreply.github.com>
2024-01-26 20:28:37 -0700

    coded out the rest of app

--- src/Components/HorizontalScrollbar.jsx
+++ src/Components/HorizontalScrollbar.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 import { Box, Typography } from '@mui/material';
 
-// import ExerciseCard from './ExerciseCard';
+import ExerciseCard from './ExerciseCard';
 import BodyPart from './BodyPart';
 import RightArrowIcon from '../assets/icons/right-arrow.png';
 import LeftArrowIcon from '../assets/icons/left-arrow.png';
@@ -28,17 +28,18 @@ const RightArrow = () => {
 };
 
 const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
-  <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+  <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} 
+  
+  >
     {data.map((item) => (
       <Box
         key={item.id || item}
         itemId={item.id || item}
         title={item.id || item}
         m="0 40px"
+        
       >
-        {bodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> : ""
-        // <ExerciseCard exercise={item} 
-         }
+        {bodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> : <ExerciseCard exercise={item} /> }
       </Box>
     ))}
   </ScrollMenu>
