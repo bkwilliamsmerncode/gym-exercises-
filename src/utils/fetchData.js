d331cb198d8b4237c107597158016ff485a59a5a
Brian Williams <152632135+bkwilliamsmerncode@users.noreply.github.com>
2024-02-20 21:13:08 -0700

    commit

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,18 +1,20 @@
 export const exerciseOptions = {
         method: 'GET',
         url: 'https://exercisedb.p.rapidapi.com/exercises',
-        params: {limit: '1300'},
+        params: {limit: '1400'},
         headers: {
-          'X-RapidAPI-Key': '830a535306mshb17da78c42d5e7dp18245ajsnc5de813368e8',
+          'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
           'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
         }
       };
 
       export const youtubeOptions = {
         method: 'GET',
+        url: 'https://youtube-search-and-download.p.rapidapi.com/channel/about',
+       
         headers: {
-            'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-            'X-RapidAPI-Key': '830a535306mshb17da78c42d5e7dp18245ajsnc5de813368e8'
+            'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com' ,
+            'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY
         }
       }
 
